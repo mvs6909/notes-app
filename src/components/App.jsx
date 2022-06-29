b692d867e2388e2c2083da2d97ec40c002cf0117
Manav Shah <mvs6909@psu.edu>
2022-06-29 17:07:07 -0400

    created notes props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notesData from "../notesData";
+
+function createNote(note){
+    return (
+        <Note 
+        title={note.title}
+        content = {note.content}
+        />
+    )
+}
 
 function App(){
     return (<div>
         <Header />
+        {notesData.map(createNote)}
         <Footer />
-        <Note />
     </div>)
 }
 
